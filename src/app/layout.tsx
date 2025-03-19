4160e6892a6031f40d2424f6e4bb0d2777246f67
Dhayanandhan <dhayaec@gmail.com>
2025-03-19 10:29:31 +0530

    remove font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 "use client";
 import { store } from "@/redux/store";
 // import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Provider } from "react-redux";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -22,9 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <div className="container my-2 mx-auto px-4 sm:px-6 lg:px-8 bg-gray-100">
           <Provider store={store}>{children}</Provider>
         </div>
